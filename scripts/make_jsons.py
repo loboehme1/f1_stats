8e22f227848012e2c4f22ce8c8921ccd4fc78c7f
Lorena <lorena.boehme@gmail.com>
2025-11-17 05:21:28 +0100

    added race data

--- scripts/make_jsons.py
+++ scripts/make_jsons.py
@@ -92,6 +92,9 @@ def color_constr(constr_id):
 # fetch basic driver data from results --> this gives us the actual number instead of just the permanent number
     # id, name, team, number, nationality, dob, (assets, colors)
 def fetch_driver_data(SEASON):
+
+    print("Fetch driver data")
+
     url = f"https://api.jolpi.ca/ergast/f1/{SEASON}/last/results/"
 
     response = requests.get(url)
@@ -140,6 +143,9 @@ def fetch_driver_data(SEASON):
 
 
 def fetch_constr_data(SEASON):
+
+    print("Fetch constructor data")
+
     url = f"https://api.jolpi.ca/ergast/f1/{SEASON}/last/constructorstandings/"
 
     response = requests.get(url)
@@ -178,10 +184,11 @@ def fetch_constr_data(SEASON):
 
 # fetch current standings data fro driverStandings
     # position, points, wins
-def fetch_standings(SEASON):
+def fetch_driver_standings(SEASON):
+
+    print("Fetch driver standings")
 
     driver_standings = {}
-    constr_standings = defaultdict(list)
 
     url = f"https://api.jolpi.ca/ergast/f1/{SEASON}/last/driverStandings/"
 
@@ -196,7 +203,6 @@ def fetch_standings(SEASON):
         constr_info = driver.get('Constructors', {})
 
         driver_id = driver_info.get('code', '')
-        constr_id = constr_info[0].get('constructorId')
 
         # data to add for standings
         pos = driver.get("position")
@@ -211,21 +217,30 @@ def fetch_standings(SEASON):
         }
 
         driver_standings[driver_id] = entry
-        constr_standings[constr_id].append(entry)
 
-    return driver_standings, constr_standings
+    return driver_standings
 
 
 # fetch results for each round for each driver from results
     # podium, round, race name, date, points, finish position, starting position, status (dnf, finsih)
 def fetch_results(SEASON, ROUNDS):
 
+    print("Fetch results")
+
+    # store race information
+    race_info = defaultdict(list)
+    results_info = defaultdict(list)
+    fastest_lap_info = defaultdict(list)
+
+    # store results per race for drivers and constructors
     driver_results = defaultdict(list)
     constr_results = defaultdict(list)
 
+    # store podium info for constructors and drivers
     podiums_driver = {}
     podiums_constr = {}
 
+    # go through each race
     for race in range(ROUNDS):
 
         url = f"https://api.jolpi.ca/ergast/f1/{SEASON}/{race+1}/results/"
@@ -233,6 +248,7 @@ def fetch_results(SEASON, ROUNDS):
         response = requests.get(url)
         results = response.json()['MRData']['RaceTable']['Races']
 
+        # check if race data has been published yet
         if not results:
             print(f'No entry for round {race+1}.')
             continue
@@ -240,42 +256,64 @@ def fetch_results(SEASON, ROUNDS):
         results = results[0]
 
         # general race info
+
         round_no = results.get('round', '')
         race_name = results.get('raceName', '')
-        date = results.get('date', '')
+        race_date = results.get('date', '')
+        race_time = results.get('time', '')
+
+        circuit = results.get('Circuit')
+        circuit_id = circuit.get('circuitId')
+        circuit_name = circuit.get('circuitName')
+
+        race_location = circuit.get('Location')
+        race_loaction = race_location.get('locality')
+        race_country = race_location.get('country')
 
 
         races = results.get('Results', [])
 
         race_points = defaultdict(int)
 
+        # store results per race per driver
+        results_race = []
+
+        # store fastest lap info
+        fastest_laps = []
+        
+        # go through each driver (position) of the race
         for position in races:
 
-            driver_id = position.get('Driver', {}).get('code', '')
-            constr_id = position.get('Constructor', {}).get('constructorId', '')
+            # driver and constr id
+            driver = position.get('Driver', {})
+            driver_id = driver.get('code', '')
+            constr = position.get('Constructor', {})
+            constr_id = constr.get('constructorId', '')
 
 
-            # race data drivers
+            # race data drivers for individual drivers json
             points = int(position.get('points', 0))
             end_pos = position.get('position', '')
             start_pos = position.get('grid', '')
+            laps_done = position.get('laps', '')
             status = position.get('status', '')
             if status == 'Lapped':
                 status = 'Finished'
 
-
-            entry = {
+            entry_driver = {
                 'round': round_no,
                 'raceName': race_name,
-                'date': date,
+                'date': race_date,
                 'points': int(points),
                 'endPos': int(end_pos),
                 'startPos': int(start_pos),
                 'status': status
             }
 
-            driver_results[driver_id].append(entry)
+            driver_results[driver_id].append(entry_driver)
 
+
+            # calculate podium data
             if race == 0:
                 podiums_driver.setdefault(driver_id, 0)
                 podiums_constr.setdefault(constr_id, 0)
@@ -286,16 +324,159 @@ def fetch_results(SEASON, ROUNDS):
 
             race_points[constr_id] += points
 
-            for constr_id, total_points in race_points.items():
-                entry = {
-                    'round': round_no,
-                    'raceName': race_name,
-                    'date': date,
-                    'points': total_points,
-                }
-                constr_results[constr_id].append(entry)
 
-    return driver_results, constr_results, podiums_driver, podiums_constr
+            # driver data for race json
+
+            driver_number = position.get('number')
+            driver_name = f"{driver.get('givenName')} {driver.get('familyName')}"
+            driver_dob = driver.get('dateOfBirth')
+            driver_nation = driver.get('nationality')
+
+            driver_info = {'driver_id': driver_id, 'name': driver_name, 'number': driver_number, 'dob': driver_dob, 'nationality': driver_nation}
+
+            # constructor info for race json
+            constr_name = constr.get('name')
+            constr_nation = constr.get('nationality')
+
+            constr_info = {'constr_id': constr_id, 'name': constr_name, 'nationality': constr_nation}
+
+            # fastest lap for race json
+            fastest_lap_data = None
+            fastest_lap_obj = position.get('FastestLap')
+
+            if fastest_lap_obj:
+                rank = fastest_lap_obj.get('rank')
+                time = fastest_lap_obj['Time'].get('time')
+                # save fastest lap in driver results
+                fastest_lap_data = {'rank': rank, 'time': time}
+                # save all fastest laps per circuit
+                fastest_lap_drivers = {'driver_id': driver_id, 'rank': rank, 'time': time}
+                fastest_laps.append(fastest_lap_drivers)
+
+            entry_results = {
+                'points': points, 
+                'end_pos': end_pos, 
+                'start_pos': start_pos, 
+                'completed_laps': laps_done, 
+                'status': status,
+                'fastest_lap': fastest_lap_data,
+                'driver': driver_info,
+                'constr': constr_info
+            }
+
+            results_race.append(entry_results)
+
+        # Append constructor results once per race (after processing all drivers)
+        for constr_id, total_points in race_points.items():
+            entry_constr = {
+                'round': round_no,
+                'raceName': race_name,
+                'date': race_date,
+                'points': total_points,
+            }
+            constr_results[constr_id].append(entry_constr)
+
+        # get number of laps from number of completed laps from first driver 
+        if not results_race:
+            print(f'No results for round {race+1}, skipping race data.')
+            continue
+            
+        race_laps = results_race[0].get('completed_laps')
+
+        # Find the fastest lap with rank 1
+        fastest_lap_rank1 = None
+        for lap in fastest_laps:
+            if lap.get('rank') == '1' or lap.get('rank') == 1:
+                fastest_lap_rank1 = lap
+                break
+
+        entry_race_core = {
+            'circuit_id': circuit_id,
+            'round': round_no,
+            'race_name': race_name,
+            'circuit_name': circuit_name,
+            'laps': race_laps,
+            'country': race_country,
+            'city': race_loaction,
+            'date': race_date,
+            'time': race_time,
+            'fastest_lap': fastest_lap_rank1
+        }
+
+        # Store results and race info for this circuit
+        fastest_lap_info[circuit_id] = fastest_laps
+        results_info[circuit_id] = results_race
+        race_info[circuit_id] = entry_race_core
+
+    return driver_results, constr_results, podiums_driver, podiums_constr, race_info, results_info, fastest_lap_info
+
+
+def fetch_quali(SEASON, ROUNDS):
+
+    print("Fetch qualification data")
+
+    quali_results = defaultdict(list)
+
+    for race in range(ROUNDS):
+
+        url = f"https://api.jolpi.ca/ergast/f1/{SEASON}/{race+1}/qualifying/"
+
+        response = requests.get(url)
+        data = response.json()['MRData']['RaceTable']['Races']
+
+        if not data:
+            print(f'No entry for round {race+1}.')
+            continue
+
+        data = data[0]
+
+
+        qualification = data['QualifyingResults']
+
+        circuit_id = data['Circuit'].get('circuitId')
+
+        result_per_race = []
+
+        for result in qualification:
+
+            quali_pos = result.get('position')
+
+            q1 = result.get('Q1')
+            q2 = result.get('Q2')
+            q3 = result.get('Q3')
+
+            sectors = {'q1': q1, 'q2': q2, 'q3': q3}
+
+            driver = result.get('Driver')
+            driver_number = result.get('number')
+            driver_id = driver.get('code')
+            driver_name = f"{driver.get('givenName')} {driver.get('familyName')}"
+            driver_dob = driver.get('dateOfBirth')
+            driver_nation = driver.get('nationality')
+
+            driver_info = {'driver_id': driver_id, 'name': driver_name, 'number': driver_number, 'dob': driver_dob, 'nationality': driver_nation}
+
+            constr = result.get('Constructor')
+            constr_id = constr.get('constructorId')
+            constr_name = constr.get('name')
+            constr_nation = constr.get('nationality')
+
+            constr_info = {'constr_id': constr_id, 'name': constr_name, 'nationality': constr_nation}
+
+            entry_results = { 
+                'quali_pos': quali_pos, 
+                'sectors': sectors,
+                'driver': driver_info,
+                'constr': constr_info
+            }
+
+            result_per_race.append(entry_results)
+
+        quali_results[circuit_id] = result_per_race
+
+
+    return quali_results
+    
 
 
 # reorganize data and standardize team names
@@ -383,6 +564,8 @@ def normalize_constr(data):
 # combine data for individual jsons and json with all drivers
 def combine_data(SEASON, ROUNDS):
 
+    print("Combine data for driver json")
+
     ## run individual processes:
 
     #driver data
@@ -391,12 +574,17 @@ def combine_data(SEASON, ROUNDS):
     constr_info = fetch_constr_data(SEASON)
 
     #data standings
-    driver_standings, constr_standings = fetch_standings(SEASON)
+    driver_standings = fetch_driver_standings(SEASON)
 
-    #results
-    driver_results, constr_results, driver_podiums, constr_podiums = fetch_results(SEASON, ROUNDS)
+    #race results
+    driver_results, constr_results, driver_podiums, constr_podiums, race_info, results_info, lap_info = fetch_results(SEASON, ROUNDS)
+
+    # qualification results
+    quali_info = fetch_quali(SEASON, ROUNDS)
 
     ## combine drivers
+
+    print("Combine data for driver json")
 
     driver_comb_all = {}
     driver_comb_indiv = {}
@@ -407,7 +595,7 @@ def combine_data(SEASON, ROUNDS):
         driver_core_all = dict(driver_info.get(did, {"driver_id": did}))    # core info all drivers
         driver_core_indiv = dict(driver_info.get(did, {"driver_id": did}))  # core info individual drivers
 
-        st   = dict(driver_standings.get(did, {}))                          # driver standings copy
+        st = dict(driver_standings.get(did, {}))                            # driver standings copy
         st["podiums"] = driver_podiums.get(did, 0)                          # add podiums to standings
         driver_core_all["standings"] = st                                   # add standings to all drivers
         driver_core_indiv["standings"] = st                                 # add standings to individual drivers
@@ -424,6 +612,8 @@ def combine_data(SEASON, ROUNDS):
 
 
     #combine constructors
+
+    print("Combine data for constructor json")
 
     constr_comb_all = {}
     constr_comb_indiv = {}
@@ -461,20 +651,52 @@ def combine_data(SEASON, ROUNDS):
     constr_comb_norm_all = normalize_constr(constr_comb_all)
     constr_comb_norm_indiv = normalize_constr(constr_comb_indiv)
 
-    return driver_comb_norm_all, driver_comb_norm_indiv, constr_comb_norm_all, constr_comb_norm_indiv
+    # race 
+
+    print("Combine data for race json")
+
+    race_comb_all = {}
+    race_comb_indiv = {}
+
+    all_ids = chain(race_info.keys())
+    for rid in sorted(all_ids):
+        race_core_all = dict(race_info.get(rid, {}))        # core info all races
+        race_core_indiv = dict(race_info.get(rid, {}))      # core info individual races
+
+        # Get results and quali data for this circuit
+        race_results = results_info.get(rid, [])
+        quali_results = quali_info.get(rid, [])
+        lap_results = lap_info.get(rid, [])
+        
+        race_core_indiv['fastest_laps'] = lap_results
+        race_core_indiv['race_results'] = race_results
+        race_core_indiv['quali_results'] = quali_results
+        
+
+        race_comb_all[rid] = race_core_all                                  # save all constr data
+        race_comb_indiv[rid] = race_core_indiv                              # save individual constr data
+
+    race_comb_norm_all = race_comb_all
+    race_comb_norm_indiv = race_comb_indiv
+
+    return driver_comb_norm_all, driver_comb_norm_indiv, constr_comb_norm_all, constr_comb_norm_indiv, race_comb_norm_all, race_comb_norm_indiv
 
 
 def make_jsons(OUT_DIR, SEASON, ROUNDS):
+
+    print("Generate jsons")
     
     # Ensure output directories exists
     driver_path = os.path.join(OUT_DIR, "driver_data")
     constr_path = os.path.join(OUT_DIR, "constr_data")
+    race_path = os.path.join(OUT_DIR, "race_data")
 
     os.makedirs(driver_path, exist_ok=True)
     os.makedirs(constr_path, exist_ok=True)
+    os.makedirs(race_path, exist_ok=True)
     
     # combined dict results + standings, cleanded number + team
-    driver_all, driver_indiv, constr_all, constr_indiv = combine_data(SEASON, ROUNDS)    
+    driver_all, driver_indiv, constr_all, constr_indiv, race_all, race_indiv = combine_data(SEASON, ROUNDS)    
     
     # make driver jsons
     driver_all = list(driver_all.values())
@@ -484,8 +706,8 @@ def make_jsons(OUT_DIR, SEASON, ROUNDS):
     print(f"Wrote {path}")
 
 
-    for code, data in driver_indiv.items():
-        path = os.path.join(driver_path, f"driver_json_{code}.json")
+    for id, data in driver_indiv.items():
+        path = os.path.join(driver_path, f"driver_json_{id}.json")
         with open(path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=False)
         print(f"Wrote {path}")
@@ -498,8 +720,20 @@ def make_jsons(OUT_DIR, SEASON, ROUNDS):
     print(f"Wrote {path}")
 
 
-    for code, data in constr_indiv.items():
-        path = os.path.join(constr_path, f"constr_json_{code}.json")
+    for id, data in constr_indiv.items():
+        path = os.path.join(constr_path, f"constr_json_{id}.json")
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=False)
+        print(f"Wrote {path}")
+
+    race_all = list(race_all.values())
+    path = os.path.join(race_path, f"race_json_all.json")
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(race_all, f, indent=2, ensure_ascii=False)
+    print(f"Wrote {path}")
+
+    for id, data in race_indiv.items():
+        path = os.path.join(race_path, f"race_json_{id}.json")
         with open(path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=False)
         print(f"Wrote {path}")
